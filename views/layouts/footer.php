343dccd09657806a46e22fda1c4f315cf54482a1
ArmenMarabyan <marabyan98@mail.ru>
2019-02-18 23:23:32 +0400

    first commit

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -34,6 +34,8 @@
 		</footer>
 		</div>
 	</div>
-
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+	<script src='/template/js/common.js' defer></script>
 </body>
 </html>
--- views/layouts/header.php
+++ views/layouts/header.php
@@ -6,14 +6,10 @@
 	<!-- <meta name="csrf-token" content="{{ csrf_token() }}"> -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="/template/css/style.css">
-
 	<!-- bootstrap -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<!-- font awesome -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-	<script src='/template/js/common.js' defer></script>
 </head>
 <body>
 
